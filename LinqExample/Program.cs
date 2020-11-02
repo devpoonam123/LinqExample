5c9a4fcc9db4b128d9fa05fa87c3b8dd1e81c007
@devPoonam123 <Poonamdwi0109@gmail.com>
2020-11-02 12:00:17 +0530

    Add project files.

---
+++ LinqExample/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> values = new List<string> { "Herfawn", "gibbon", "R", "pHer",  "jackalope", "Herjack","abv" };
+
+            string First=   SingleValue1.GetFirstSingleLetterWord(values);
+            Console.WriteLine(First);
+
+            string last = SingleValue1.GetLastWordWithHerInIt(values);
+            Console.WriteLine(last);
+
+            string Index = SingleValue1.GetFifthWordIfItExists(values);
+            Console.WriteLine(Index);
+
+            string last1 = SingleValue1.GetLastWordIfAny(values);
+            Console.WriteLine(last1);
+
+
+
+
+            //List<string> animalsname = new List<string> { "fawn", "gibbon", "heron", "ibex", "jackalope" };
+
+            //IEnumerable<string> animals = from name in animalsname
+            //                              where name.Length >= 5
+            //                              orderby name.Length
+            //                              select name;
+
+            //IEnumerable<int> intvalue = new List<int> { 34, 68, 87, 1, 2, 2, 4, 7, 8, 9, 1 };
+
+
+            //IEnumerable<string> list = FilterAndSort(animalsname, "o");
+            ////IEnumerable<int> inlist= FilterEvenValue(intvalue);
+
+            //foreach (var v in list)
+            //{
+            //    Console.WriteLine(v + ",");
+            //}
+
+
+
+
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<string> FilterAndSort(IEnumerable<string> inValues, string pattern)
+        {
+            //var v = from value in inValues
+            //        where value.Contains(pattern)
+            //        orderby value
+            //        select value;
+
+            var v = inValues.Where(m =>m.Length>5 && m.Contains(pattern)).OrderBy(m => m).Select(m => m);
+            return v;
+
+        }
+
+        public static IEnumerable<int> FilterEvenValue(IEnumerable<int> values)
+        {
+            //Query Syntax
+
+            //var v = (from v1 in values
+            //        where v1 % 2 == 0
+            //        let t=v1*v1
+            //        orderby v1
+            //        select t).Distinct();
+
+            //Method Syntax
+
+            var v = values.Where(m => m % 2 == 0)
+
+                .OrderBy(m => m)
+                .Select(m => m * m).Distinct();
+
+
+            return v;
+
+
+        }
+    }
+}
---
+++ LinqExample/SingleValue1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+   public static class SingleValue1
+    {
+        public static string GetFirstSingleLetterWord(IEnumerable<string> words)
+        {
+            var v = words.FirstOrDefault(val=>val.Length==1);
+            return v;
+        }
+
+        public static string GetLastWordWithHerInIt(IEnumerable<string> words)
+        {
+            var v = words.LastOrDefault(val => val.Contains("Her"));
+            return v;
+        }
+
+        public static string GetFifthWordIfItExists(IEnumerable<string> words)
+        {
+            var v = words.ElementAtOrDefault(9-1);
+            return v;
+        }
+
+        public static string GetLastWordIfAny(IEnumerable<string> words)
+        {
+            var v = words.LastOrDefault();
+            return v;
+        }
+    }
+}
